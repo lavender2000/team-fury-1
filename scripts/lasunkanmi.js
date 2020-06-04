21dbd58699ad55e8fc5a24f9b0918b6c09547d85
unknown <olaoluwabenjamin99@gmail.com>
2020-06-04 22:22:43 +0100

    add mu script

---
+++ scripts/lasunkanmi.js
@@ -0,0 +1,8 @@
+let intro = {
+    name: 'Benjamin Olaoluwa',
+    ID: 'HNG-04958',
+    email: 'olaoluwabenjamin99@gmail.com',
+    language: 'Javascript'
+}
+
+console.log('Hello World, this is ' + intro.name + ' with HNGi7 ID ' + intro.ID + ' using ' + intro.language + ' for stage 2')
