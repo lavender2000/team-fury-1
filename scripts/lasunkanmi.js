a393bdb414b52bf7a5d1f22a5889e637984da610
unknown <olaoluwabenjamin99@gmail.com>
2020-06-04 21:21:29 +0100

    add my script

---
+++ scripts/lasunkanmi.js
@@ -0,0 +1,9 @@
+let introduction = {
+    name: 'Benjamin Olaoluwa',
+    ID: 'HNG-04958',
+    language: 'javascript',
+    email: 'olaoluwabenjamin99@gmail.com',
+
+}
+
+console.log('Hello World, this is ' + introduction.name + ' with HNGi7 ID ' + introduction.ID + ' using ' + introduction.language + ' for stage 2 task');
